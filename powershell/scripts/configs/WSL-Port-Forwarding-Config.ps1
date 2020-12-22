8d145a69b4123b6a12a73bcd080401e30189fb0f
Andrius Solopovas <andrius.solopovas@gmail.com>
2020-12-22 16:52:04 +0000

    save

--- powershell/scripts/configs/WSL-Port-Forwarding-Config.ps1
+++ powershell/scripts/configs/WSL-Port-Forwarding-Config.ps1
@@ -1,3 +1,23 @@
 $ipAddress = wsl ip addr show dev eth0 | wsl sed -u -n 3p | wsl awk '{print \$2}' | wsl cut -d / -f1
-netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=22 connectaddress=$ipAddress connectport=22
-netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=3000 connectaddress=$ipAddress connectport=3000
+$ports = 22,80,443,3000
+$arg=$args[0]
+
+if ($arg -eq "reset") {
+    Write-Output "Resetting portproxy configuration"
+    netsh int portproxy reset all
+    foreach ($port in $ports) {
+        Write-Output "Removing WSL firewall rule `"Port $port`""
+        Remove-NetFirewallRule -DisplayName "Port $port"
+    }
+}
+
+if ($arg -eq "show") {
+    netsh interface portproxy show v4tov4
+}
+
+if ($arg -eq "setup") {
+    foreach ($port in $ports) {
+        netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=$port connectaddress=$ipAddress connectport=$port
+        New-NetFirewallRule -DisplayName "Port $port" -Direction Inbound -LocalPort $port -Protocol TCP -Action Allow -Profile Private
+    }
+}
