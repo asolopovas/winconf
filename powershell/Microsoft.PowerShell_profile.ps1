745b94007eb6f563a73773c38bb8187fa80c7491
Andrius Solopovas <andrius.solopovas@gmail.com>
2023-06-11 21:03:59 +0100

    save

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,7 @@ $power_shell_dir = "$root\powershell"
 $ENV:STARSHIP_CONFIG = "$power_shell_dir\starship.toml"
 
 . $root\functions.ps1
+. $power_shell_dir\completions\git-cli.ps1
 . $power_shell_dir\remove-aliases.ps1
 . $power_shell_dir\shortcuts.ps1
 
