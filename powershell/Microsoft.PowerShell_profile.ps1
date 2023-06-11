0f1c0368312dc1d25447dd99432ba57a787e8f50
Andrius Solopovas <andrius.solopovas@gmail.com>
2023-06-11 20:50:38 +0100

    save

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,6 @@
 $root = "$env:USERPROFILE\winconf"
 $power_shell_dir = "$root\powershell"
 $ENV:STARSHIP_CONFIG = "$power_shell_dir\starship.toml"
-$ENV:SPACESHIP_PROMPT_ADD_NEWLINE = $false
-$ENV:SPACESHIP_PROMPT_SEPARATE_LINE = $false
-$ENV:SPACESHIP_RPROMPT_ADD_NEWLINE = $true
 
 . $root\functions.ps1
 . $power_shell_dir\remove-aliases.ps1
