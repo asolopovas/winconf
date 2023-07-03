fecf8be3df967819d697021ba420b536fc17a0e1
Andrius Solopovas <andrius.solopovas@gmail.com>
2023-07-03 23:43:59 +0100

    save

--- powershell/scripts/Sync-DirectoryOpus.ps1
+++ powershell/scripts/Sync-DirectoryOpus.ps1
@@ -1,7 +1,7 @@
 . $env:userprofile\winconf\functions.ps1
 
 $src = "$env:APPDATA\GPSoftware\Directory Opus"
-$target = "$env:USERPROFILE\Insync\andrius.solopovas@gmail.com\Google Drive\configs\Directory Opus"
+$target = "$env:USERPROFILE\gdrive\configs\Directory Opus"
 
 if (Test-Path $src -PathType Container) {
     Remove-Item -Force $src -Recurse
