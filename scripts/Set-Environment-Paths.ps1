6c7bc766a11c46e0681912bae27e01e5c715ce79
Andrius Solopovas <andrius.solopovas@gmail.com>
2023-07-10 18:05:06 +0100

    save

--- scripts/Set-Environment-Paths.ps1
+++ scripts/Set-Environment-Paths.ps1
@@ -2,10 +2,12 @@ $paths = @(
    $env:LOCALAPPDATA + "\Microsoft\WinGet\Links"
     "C:\Program Files\starship\bin\"
     $env:USERPROFILE + "\winconf\bin"
+    $env:USERPROFILE  + "\miniconda3"
 )
 
 foreach ($path in $paths) {
     if ($env:PATH -notlike "*$path*") {
+        Write-Host "Adding $path to System PATH ..."
         [Environment]::SetEnvironmentVariable("Path", $env:Path + ";$path", "User")
     }
 }
