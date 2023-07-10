1089f89e2588baa53c5b3add764f734ecb0ac957
Andrius Solopovas <andrius.solopovas@gmail.com>
2023-07-11 00:28:20 +0100

    save

--- powershell/profile.ps1
+++
@@ -1,8 +0,0 @@
-
-#region conda initialize
-# !! Contents within this block are managed by 'conda init' !!
-If (Test-Path "C:\Users\asolo\miniconda3\Scripts\conda.exe") {
-    (& "C:\Users\asolo\miniconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | ?{$_} | Invoke-Expression
-}
-#endregion
-
