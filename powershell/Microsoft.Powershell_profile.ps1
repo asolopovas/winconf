fd17ee184ea88efd5d47ce41ec138bb0a76e7f7a
Andrius Solopovas <andrius.solopovas@gmail.com>
2024-04-20 15:19:52 +0100

    save

--- powershell/Microsoft.Powershell_profile.ps1
+++ powershell/Microsoft.Powershell_profile.ps1
@@ -2,6 +2,9 @@ $root = "$env:USERPROFILE\winconf"
 $power_shell_dir = "$root\powershell"
 $ENV:STARSHIP_CONFIG = "$power_shell_dir\starship.toml"
 
+. $env:USERPROFILE\miniconda3\shell\condabin\conda-hook.ps1
+conda activate $env:USERPROFILE\miniconda3
+
 . $root\functions.ps1
 . $power_shell_dir\completions\git-cli.ps1
 . $power_shell_dir\remove-aliases.ps1
@@ -14,4 +17,13 @@ if (-not (Get-Module -Name $moduleName)) {
 }
 
 . $power_shell_dir\starship.ps1
+
+
+
+$power_toys_module = "$env:LOCALAPPDATA\PowerToys\WinGetCommandNotFound.psd1"
+# test if file exists import if it does
+if (Test-Path $power_toys_module) {
+    Import-Module $power_toys_module
+}
+
 
