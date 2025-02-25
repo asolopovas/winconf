a440cc2aefee364ac05e50ff185c4b1226c90be2
Andrius Solopovas <andrius.solopovas@gmail.com>
2025-02-25 17:04:59 +0000

    save

--- init-software.ps1
+++ init-software.ps1
@@ -1,4 +1,4 @@
 $initScript = "$env:TEMP\init.ps1"
 Invoke-WebRequest -Uri "https://raw.githubusercontent.com/asolopovas/winconf/refs/heads/main/init.ps1" -OutFile $initScript
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-powershell -ExecutionPolicy Bypass -File $initScript -Software $true
+Start-Process -FilePath "powershell" -ArgumentList "-ExecutionPolicy Bypass -File `"$initScript`" -Software" -Wait -NoNewWindow
