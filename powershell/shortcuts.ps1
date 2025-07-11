95fd6a424fdbd2f509c41b112b79ac2fc954262b
Andrius Solopovas <andrius.solopovas@gmail.com>
2025-07-11 10:28:09 +0100

    improved general perfomance and excluded various garbage

--- powershell/shortcuts.ps1
+++ powershell/shortcuts.ps1
@@ -1,8 +1,21 @@
 if (Get-Module -Name PSFzf -ListAvailable) {
     Import-Module PSFzf -ErrorAction SilentlyContinue
-    
+
     $env:FZF_DEFAULT_OPTS = '--height 40% --layout=reverse --border'
-    $env:FZF_ALT_C_COMMAND = 'fd --type directory --hidden --exclude AppData --exclude .git --exclude .vs --exclude node_modules --exclude .vscode --exclude "Application Data" --exclude "Local Settings" --exclude Cookies --exclude Recent --exclude SendTo --exclude "Start Menu" --exclude Templates --exclude NetHood --exclude PrintHood --exclude "Temporary Internet Files" --exclude "3D Objects" --exclude "Saved Games" --exclude Links --exclude Favorites --exclude Contacts --exclude Searches'
-    
+
+    $excludeDirs = @(
+        'AppData', '.git', '.vs', 'node_modules', '.vscode',
+        'Application Data', 'Local Settings', 'Cookies', 'Recent',
+        'SendTo', 'Start Menu', 'Templates', 'NetHood', 'PrintHood',
+        'Temporary Internet Files', '3D Objects', 'Saved Games',
+        'Links', 'Favorites', 'Contacts', 'Searches',
+        '.cache', '.conda', '.cursor', '.bun', '.aws', '.azure',
+        '.amp', '.docker', '.local', '.redhat', '.ScreamingFrogSEOSpider',
+        'vscode-remote-wsl', 'wslu', 'miniconda3'
+    )
+
+    $excludeFlags = $excludeDirs | ForEach-Object { "--exclude `"$_`"" }
+    $env:FZF_ALT_C_COMMAND = "fd --type directory --max-depth 4 $($excludeFlags -join ' ')"
+
     Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+r' -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordSetLocation 'Alt+c'
 }
