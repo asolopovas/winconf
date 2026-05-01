b1afa39a65ab44b402115c4ce912de4934b576cc
Andrius Solopovas <andrius.solopovas@gmail.com>
2026-05-01 23:37:01 +0100

    save

--- powershell/modules/aliases/package-managers.ps1
+++ powershell/modules/aliases/package-managers.ps1
@@ -1,10 +1,3 @@
 # Composer
 function ci { composer install }
 function cr { composer require $args }
-
-# pnpm
-function pi { pnpm install $args }
-function pu { pnpm update $args}
-
-# Poetry
-function pr { poetry run $args }
